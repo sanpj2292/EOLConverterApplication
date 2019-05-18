0fe1f40afb81a8392b135f3ec25472275d3ff335
Sai Sankeerth <sankeerth2292@gmail.com>
2019-05-18 22:37:38 +0530

    refs #master Documentation for methods, class commit

--- eol_converted_app.py
+++ eol_converted_app.py
@@ -5,10 +5,14 @@ from time import time
 
 class ArgInitializer(ArgumentParser):
 
+    # Constructor for ArgInitializer
     def __init__(self):
         super(ArgInitializer, self).__init__()
         self._add_options()
-
+    
+    '''
+        Read the command line argument and store it in the attributes of ArgInitializer Class
+    '''
     def _add_options(self):
         self.add_help = True
         self.description = 'EOL Converter Application'
@@ -22,9 +26,20 @@ class ArgInitializer(ArgumentParser):
         self.add_argument('-rcz','--readChunkSz',type=int,
                 help='Multiple of 1024 Bytes while reading the data(Helpful in LazyRead of large data)')
 
+'''
+EOLConverter class is used as an object to enable the conversion of line endings in a file
 
+Parameters:
+    file - Target File path relative/absolute including the Filename
+    eol - The desired line-ending to which the file needs to be converted to
+    rcz - To enable efficient reading of large files we need to specify this
+          This would help the program execute the reading of contents of file in
+          lazy manner(type:int)
+          For ex: if rcz is given as 10, then 10 kb data will be read in a single go
+'''
 class EOLConverter(object):
 
+    # Constructor for EOLConverter class
     def __init__(self, arg_init):
         # BugFix in regex matching
         self.MATCH_REGEX = b'(\n)' if arg_init.eol == 'CRLF' else b'(\r\n)'
@@ -35,22 +50,26 @@ class EOLConverter(object):
         self._ext = path.splitext(self.file)[1][1:].strip()
         self._should_write = True
     
+    '''
+        Generator for lazy read of large files
+    '''
     def lazy_read(self, fileObj, chunkSize=4096):
-        """
-        Lazy function to read a file piece by piece.
-        Default chunk size: 4kB.
-        """
         while True:
             data = fileObj.read(chunkSize)
             if not data:
                 break
             yield data
     
+    '''
+        Get folder name of the pseudo target-file
+    '''
     def _get_pseud_conv_fDet(self):
         ex_fnm = self.file.split(path.sep)[-1]
         f_nm = ex_fnm.split('.')[0]
         return self._get_folder_name(), f_nm + '_conv.'+self._ext
-
+    '''
+        Reads the existing content from the target file    
+    '''
     def read_file(self):
         st = time()
         fo_nm, cf_nm = self._get_pseud_conv_fDet()
@@ -76,13 +95,16 @@ class EOLConverter(object):
             remove(conv_abs_path)
         end = time()
         print('Execution Time For Reading & Writing in new File %.8f' % (end - st))
-
+    
+    '''
+        Gets the folder name of the target file
+    '''
     def _get_folder_name(self):
-        '''
-            Returns the folder name, given a full folder path
-        '''
         return self.file.split(path.sep)[-2]
 
+    '''
+        Writes to the original file
+    '''
     def write_file(self):
         if self._should_write:
             st = time()
@@ -94,7 +116,10 @@ class EOLConverter(object):
             rename(abs_fpath, path.abspath(self.file))
             end = time()
             print('Execution Time in Writing the Read File in the earlier Read File %.8f' % (end - st))
-
+    
+    '''
+        Converts to the desired EOL for the target-file
+    '''
     def convert_EOL(self):
         self.read_file()
         self.write_file()
