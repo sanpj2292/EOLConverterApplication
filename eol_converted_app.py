081b994ce5dcb18518fc35bbf3e6e2abcf0460d5
Sai Sankeerth <sankeerth2292@gmail.com>
2019-05-18 08:33:20 +0530

    refs #master already proper line end check addition commit

--- eol_converted_app.py
+++ eol_converted_app.py
@@ -37,8 +37,9 @@ class EOLConverter(object):
         self.file = arg_init.file
         self.eol = arg_init.eol
         self._ext = path.splitext(self.file)[1][1:].strip()
+        self._should_write = True
     
-    def readInChunks(self, fileObj, chunkSize=4096):
+    def lazy_read(self, fileObj, chunkSize=4096):
         """
         Lazy function to read a file piece by piece.
         Default chunk size: 4kB.
@@ -48,30 +49,49 @@ class EOLConverter(object):
             if not data:
                 break
             yield data
+    
+    def _get_pseud_conv_fDet(self):
+        f_nm = re.search(r'(?<=\\)[a-zA-z0-9\+\_\-]+(?=\.)', self.file)
+        return self._get_folder_name(), f_nm.group(0) + '_conv.'+self._ext
 
     def read_file(self):
         st = time()
+        fo_nm, cf_nm = self._get_pseud_conv_fDet()
         mreg, rpreg = self.MATCH_REGEX, self.REPLACE_REG
         with open(path.abspath(self.file), 'rb') as _file:
-            with open('converted.'+self._ext,'wb') as _wf:
-                for line in self.readInChunks(_file,chunkSize=self.rcz):
+            with open(path.join(fo_nm, cf_nm),'wb') as _wf:
+                for line in self.lazy_read(_file,chunkSize=self.rcz):
                     wline = []
-                    wline.append(re.sub(mreg, rpreg, line, 0, re.DOTALL))
-                    print(wline)
-                    _wf.write(b''.join(wline))
+                    if re.search(mreg, line):      
+                        wline.append(re.sub(mreg, rpreg, line, 0, re.DOTALL))
+                        print(wline)
+                        _wf.write(b''.join(wline))
+                    else:
+                        print('The file with name {file} has CRLF EOL'.format(
+                            file=self.file
+                        ))
+                        self._should_write = False
+                        break
         end = time()
         print('Execution Time For Reading & Writing in new File %.8f' % (end - st))
 
-    def write_file(self):
-        st = time()
-        with open('converted.'+self._ext,'rb') as _rf:
-            with open(path.abspath(self.file), 'wb') as _wf:
-                for line in self.readInChunks(_rf, chunkSize=self.wcz):
-                    _wf.write(line)
-            _rf.flush()
+    def _get_folder_name(self):
+        '''
+            Returns the folder name, given a full folder path
+        '''
+        return self.file.split(path.sep)[-2]
 
-        end = time()
-        print('Execution Time in Writing the Read File in the earlier Read File %.8f' % (end - st))
+    def write_file(self):
+        if self._should_write:
+            st = time()
+            fo_nm, cf_nm = self._get_pseud_conv_fDet()
+            with open(path.join(fo_nm, cf_nm),'rb') as _rf:
+                with open(path.abspath(self.file), 'wb') as _wf:
+                    for line in self.lazy_read(_rf, chunkSize=self.wcz):
+                        _wf.write(line)
+                _rf.flush()
+            end = time()
+            print('Execution Time in Writing the Read File in the earlier Read File %.8f' % (end - st))
 
     def convert_EOL(self):
         self.read_file()
